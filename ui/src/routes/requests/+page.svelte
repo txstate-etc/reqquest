f6006bac3ba456acf031a26901946d1e32b495da
Nick Wing <nwing@txstate.edu>
2025-06-10 11:54:26 -0500

    better performance for app request list

--- ui/src/routes/requests/+page.svelte
+++ ui/src/routes/requests/+page.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
   import { ColumnList, FilterUI } from '@txstate-mws/carbon-svelte'
   import View from 'carbon-icons-svelte/lib/View.svelte'
-  import { htmlEncode, sortby } from 'txstate-utils'
+  import { htmlEncode, keyby, sortby } from 'txstate-utils'
   import { uiRegistry } from '../../local/index.js'
   import type { PageData } from './$types.js'
 
   export let data: PageData
-  $: ({ appRequests: requests, appRequestIndexes: indexes } = data)
+  $: ({ appRequests, appRequestIndexes: indexes } = data)
+  $: requests = appRequests.map(r => ({ ...r, indexByCat: keyby(r.indexCategories, 'category') }))
   $: indexColumns = sortby(indexes.filter(idx => idx.appRequestListPriority), 'appRequestListPriority').map(idx => ({
     id: idx.category,
     label: idx.categoryLabel,
-    render: (r: PageData['appRequests'][number]) => htmlEncode(r.indexCategories.find(c => c.category === idx.category)?.values.map(v => v.label).join(', '))
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- r.indexByCat[idx.category] may be undefined if no values are present
+    render: (r: typeof requests[number]) => htmlEncode(r.indexByCat[idx.category]?.values.map(v => v.label).join(', '))
   }))
   $: filterIndexes = sortby(indexes.filter(idx => idx.listFiltersPriority), 'listFiltersPriority')
 </script>
