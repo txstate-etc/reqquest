de6eb3e7a5fc82ab2c269dcdedbf212cfca37349
Joshua Howard <j_h556@txstate.edu>
2025-08-12 16:53:17 -0500

    Recurring prompt update test for checking available and unanswered prompts.  In work

--- test/tests/apprequests.test.ts
+++ test/tests/apprequests.test.ts
@@ -1,5 +1,18 @@
+import { Hash } from 'crypto'
 import { expect, test } from './fixtures.js'
 import { DateTime } from 'luxon'
+import { YardData } from '../../testserver/src/definitions/yard.js'
+import { CatTowerData } from '../../testserver/src/definitions/cat.js'
+
+const promptMapPass: Map<string, Map<string, any>> = new Map([
+  ['have_yard_prompt', new Map<string, YardData>([['pass_0', { haveYard: true, squareFootage: 1000, totalPets: 2 }]])],
+  ['have_a_cat_tower_prompt', new Map<string, CatTowerData>([['pass_0', { haveCatTower: true }], ['pass_1', { haveCatTower: false, willPurchaseCatTower: true }]])]
+])
+
+const promptMapFail: Map<string, Map<string, any>> = new Map([
+  ['have_yard_prompt', new Map<string, YardData>([['fail_0', { haveYard: false }]])],
+  ['have_a_cat_tower_prompt', new Map<string, CatTowerData>([['fail_0', { haveCatTower: false, willPurchaseCatTower: false }]])]
+])
 
 test.describe('App Request workflows', () => {
   const name = '2025 app-req'
@@ -170,4 +183,82 @@ test.describe('App Request workflows', () => {
     const { createAppRequest } = await applicantRequest.graphql<{ createAppRequest: { appRequest: { id: number, applicant: { login: string } }, messages: { message: string }[] } }>(query, variables)
     expect(createAppRequest.messages[0].message.includes('A request already exists in')).toEqual(true)
   })
+  test('Sudoer - create app request on behalf of applicant after applicant already created own', async ({ suRequest }) => {
+    const query = `
+      mutation CreateAppRequest($login:String!, $periodId: ID!, $validateOnly: Boolean) {
+        createAppRequest(login: $login, periodId:$periodId, validateOnly: $validateOnly) {
+          appRequest {
+            id
+            applicant {
+              login
+            }
+          }
+          messages{
+            message
+          }
+        }
+      }
+    `
+    const variables = { login: applicantLogin, periodId, validateOnly: false }
+    const { createAppRequest } = await suRequest.graphql<{ createAppRequest: { appRequest: { id: number, applicant: { login: string } }, messages: { message: string }[] } }>(query, variables)
+    expect(createAppRequest.messages[0].message.includes('A request already exists in')).toEqual(true)
+  })
+
+  /** TEST: 'Applicant - update available prompt' is a large recurring test that exercises prompts that are not answered and become available */
+  let availableUnasweredPromptsExist = true
+  let promptIteration = 0
+  while (availableUnasweredPromptsExist) {
+    test(`Applicant - update available and not answered prompt ${promptIteration} with passing data`, async ({ applicantRequest }) => {
+      promptIteration++
+      const query_get_prompts = `
+        query GetPromptsForRequest($appRequestIds: [ID!]) {
+          appRequests(filter: {ids:$appRequestIds}) {
+            applications {
+              programKey
+              requirements {
+                smartTitle
+                prompts {
+                  id
+                  key
+                  title
+                  navTitle
+                  answered
+                  visibility
+                }
+              }
+            }
+          }
+        }
+      `
+      const query_update_prompt = `
+        mutation UpdatePrompt($promptId: ID!,$data: JsonData!,$validateOnly: Boolean, $dataVersion: Float){
+          updatePrompt(promptId:$promptId, data:$data, validateOnly:$validateOnly, dataVersion: $dataVersion){
+            success
+            messages {
+              message
+              type
+              arg
+            }
+          }
+        }
+      `
+      const query_get_prompt_variables = { appRequestIds: [appRequestId] }
+      const { appRequests } = await applicantRequest.graphql<{ appRequests: { applications: { programKey: string, requirements: { smartTitle: string, prompts: { id: number, key: string, answered: string, visibility: string }[] }[] }[] }[] }>(query_get_prompts, query_get_prompt_variables)
+      expect(appRequests[0].applications.length).toBeGreaterThanOrEqual(1)
+
+      const allAvailableUnansweredPrompts = appRequests.flatMap(appReq => appReq.applications.flatMap(app => app.requirements.flatMap(req => req.prompts.filter(prompt => !prompt.answered && prompt.visibility === 'AVAILABLE'))))
+      if (allAvailableUnansweredPrompts.length < 1) availableUnasweredPromptsExist = false
+      // update unanswered available prompts
+      for (const availablePrompt of allAvailableUnansweredPrompts) {
+        const promptMap = promptMapPass.get(availablePrompt.key)
+        if (promptMap) {
+          for (const [key, value] of Object.entries(promptMap)) {
+            const query_update_prompt_variables = { promptId: availablePrompt.id, data: value, validateOnly: false }
+            const { updatePrompt } = await applicantRequest.graphql<{ updatePrompt: { success: boolean, messages: { message: string }[] } }>(query_update_prompt, query_update_prompt_variables)
+            expect(updatePrompt.success).toEqual(true)
+          }
+        }
+      }
+    })
+  }
 })
