a68047b719e6f118a196ff464568947a000719e5
Joshua Howard <j_h556@txstate.edu>
2025-09-04 15:36:36 -0500

    Remove APPROVAL req prompt data from applicant arrays

--- test/tests/promptdata.ts
+++ test/tests/promptdata.ts
@@ -6,7 +6,6 @@ export const promptMapApplicantQualified: Map<string, Map<string, any>> = new Ma
   ['have_a_cat_tower_prompt', new Map<string, CatTowerPromptData>([['pass_0', { haveCatTower: true }], ['pass_1', { haveCatTower: false, willPurchaseCatTower: true }]])],
   ['not_allergic_to_tuna_prompt', new Map<string, TunaAllergyPromptData>([['pass_0', { allergicToTuna: false }]])],
   ['must_exercise_your_dog_prompt', new Map<string, ExercisePromptData>([['pass_0', { exerciseHours: 10 }]])],
-  ['applicant_seems_nice_prompt', new Map<string, NicePromptData>([['pass_0', { seemsNice: true }]])],
   ['other_cats_prompt', new Map<string, OtherCatsPromptData>([['pass_0', { hasOtherCats: false }]])]
 ])
 
@@ -16,7 +15,6 @@ export const promptMapApplicantUnqualified: Map<string, Map<string, any>> = new 
   ['have_a_cat_tower_prompt', new Map<string, CatTowerPromptData>([['fail_0', { haveCatTower: false, willPurchaseCatTower: false }]])],
   ['not_allergic_to_tuna_prompt', new Map<string, TunaAllergyPromptData>([['fail_0', { allergicToTuna: true }]])],
   ['must_exercise_your_dog_prompt', new Map<string, ExercisePromptData>([['fail_0', { exerciseHours: 1 }]])],
-  ['applicant_seems_nice_prompt', new Map<string, NicePromptData>([['fail_0', { seemsNice: false }]])],
   ['other_cats_prompt', new Map<string, OtherCatsPromptData>([['fail_0', { hasOtherCats: true }]])]
 ])
 
