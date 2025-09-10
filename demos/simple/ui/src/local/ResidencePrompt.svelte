4cc786c7d75ab38a4d04ffb9a9deb548a9598390
Joshua Howard <j_h556@txstate.edu>
2025-09-10 16:14:38 -0500

    Legend text change

--- demos/simple/ui/src/local/ResidencePrompt.svelte
+++ demos/simple/ui/src/local/ResidencePrompt.svelte
@@ -4,7 +4,7 @@
   export let fetched: { residentOfState: string }
   console.log('Residence UI prompt')
   console.log(fetched)
-  $: legendText = `GO Are you a resident of ${fetched.residentOfState}?`  
+  $: legendText = `Are you a resident of ${fetched.residentOfState}?`  
 </script>
 
 <FieldRadio boolean path="residentOfRequiredState" legendText="{legendText}" items={[{ label: 'Yes', value: true }, { label: 'No', value: false }]} />
