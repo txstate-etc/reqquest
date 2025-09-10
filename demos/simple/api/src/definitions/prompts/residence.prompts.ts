ff1397b784b8e2ca4c9a1e5bb38cb4f4d607c74d
Joshua Howard <j_h556@txstate.edu>
2025-09-10 17:40:36 -0500

    'Simple' demo dynamic residency prompt

--- demos/simple/api/src/definitions/prompts/residence.prompts.ts
+++ demos/simple/api/src/definitions/prompts/residence.prompts.ts
@@ -7,10 +7,7 @@ export const state_residence_prompt: PromptDefinition = {
   title: 'State residency',
   description: 'Applicant will identify if they reside in the required state.',
   schema: StateResidencePromptSchema,
-  fetch: async (data, config) => { console.log(`***FETCH CONFIG***${JSON.stringify(config)}`); return ({ residentOfState: config.residentOfState })},
   validate: (data, config, allConfig) => {
-    console.log(`***VALIDATE CONFIG***${JSON.stringify(config)}`); 
-    console.log(`***ALL CONFIG***${JSON.stringify(allConfig)}`); 
     const messages: MutationMessage[] = []
     if (data.residentOfRequiredState == null) {
       messages.push({ type: MutationMessageType.error, message: 'Please confirm specify you are a resident of the state.', arg: 'residentOfRequiredState' })
--- demos/simple/ui/src/local/ResidencePrompt.svelte
+++ demos/simple/ui/src/local/ResidencePrompt.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
   import { FieldRadio } from '@txstate-mws/carbon-svelte'
-
-  export let fetched: { residentOfState: string }
-  console.log('Residence UI prompt')
-  console.log(fetched)
-  $: legendText = `Are you a resident of ${fetched.residentOfState}?`  
+  export let configData
+  $: legendText = `Are you a resident of ${configData.state_residence_req.residentOfState}?`  
 </script>
-
 <FieldRadio boolean path="residentOfRequiredState" legendText="{legendText}" items={[{ label: 'Yes', value: true }, { label: 'No', value: false }]} />
