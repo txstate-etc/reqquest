df75595889c9664dbc3a3d92155ab04b486578d6
Brian West <kny38@txstate.edu>
2025-09-17 16:23:40 -0500

    ui create generic intro panel

---
+++ ui/src/lib/components/IntroPanel.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let title = ''
+  export let subtitle = ''
+</script>
+
+<section class="bg-[var(--cds-ui-01)] p-4 text-2xl">
+  <div class="repel">
+    <div class="max-w-lg">
+      {#if title}
+        <h2 class="text-[1.25rem] text-[var(--cds-text-01,#1A1A1A)] font-normal leading-7">
+          {title}
+        </h2>
+        {#if subtitle}
+          <p class="text-sm text-[var(--cds-text-02)] leading-[18px] tracking-[0.16px]">
+            {subtitle}
+          </p>
+        {/if}
+      {/if}
+      <!-- Optional content slot below title/subtitle -->
+      {#if $$slots.default}
+        <div class="mt-2">
+          <slot />
+        </div>
+      {/if}
+    </div>
+    {#if $$slots['block-end']}
+    <div class="block-end-slot">
+      <slot name="block-end" />
+    </div>
+    {/if}
+  </div>
+</section>
--- ui/src/lib/components/index.ts
+++ ui/src/lib/components/index.ts
@@ -1,4 +1,4 @@
 export { default as ButtonLoadingIcon } from './ButtonLoadingIcon.svelte'
 export { default as ApplicationDetailsView } from './ApplicationDetailsView.svelte'
 export { default as AppRequestCard } from './AppRequestCard.svelte'
-export type { DashboardAppRequest, ApplicationDetailsData } from './types.js'
+export { default as IntroPanel } from './IntroPanel.svelte'
