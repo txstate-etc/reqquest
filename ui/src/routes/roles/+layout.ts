a6135461acf0248e22265ac151771259862832ce
Charles Tabor <ct37@txstate.edu>
2025-09-19 14:26:31 -0500

    ui fix tabs for users

--- ui/src/routes/roles/+layout.ts
+++ ui/src/routes/roles/+layout.ts
@@ -3,7 +3,8 @@ import type { LayoutLoad } from './$types'
 // import { unifiedAuth } from '@txstate-mws/sveltekit-utils'
 
 export const load: LayoutLoad = async input => {
-  const showTabs = (input.url.pathname.split('/').length < 3)
+  const lastPath = input.url.pathname.split('/').pop()
+  const showTabs = (lastPath === 'roles' || lastPath === 'users')
   // api.fetch = input.fetch
   // await unifiedAuth.handle(api, input)
   // const access = await api.getAccess()
