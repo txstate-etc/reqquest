75a0394a720e7288e08dd4fbf0b0a9772f10f0c5
Charles Tabor <ct37@txstate.edu>
2025-09-19 13:21:38 -0500

    ui move users to same level as roles

--- ui/src/routes/roles/+layout.svelte
+++ ui/src/routes/roles/+layout.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import { TabLinks } from '@txstate-mws/carbon-svelte'
   const tabs = [
-  { label: 'Roles', href: `/roles` },
-  { label: 'Users', href: `/roles/users` },
-]  
+    { label: 'Roles', href: `/roles` },
+    { label: 'Users', href: `/roles/users` },
+  ]
 </script>
 <div class="flow">
   <TabLinks {tabs} />
