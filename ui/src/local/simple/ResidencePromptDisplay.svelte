dc4f93e5c930048f5c40f7a39c57fbb6bb518ffc
Joshua Howard <j_h556@txstate.edu>
2025-09-25 12:04:29 -0500

    Simple demo display prompt config data notifier

--- ui/src/local/simple/ResidencePromptDisplay.svelte
+++ ui/src/local/simple/ResidencePromptDisplay.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import type { StateResidencePromptData } from './types.js'
   export let data: StateResidencePromptData
-  // export let configData
+  export let configData
   // $: resultText = `Resident homeowner of ${configData.state_residence_req.residentOfState}`  
 </script>
 {#if data.residentOfRequiredState}
-   Resident of ##configData is not available to display state##<!-- Config data does not seem to be available for display prompt {resultText}. -->
+   Resident of ##configData is { configData ? '' : 'not ' } available to display state##<!-- Config data does not seem to be available for display prompt {resultText}. -->
 {:else}
   Non resident.
 {/if}
