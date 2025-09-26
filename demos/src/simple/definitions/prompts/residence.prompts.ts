a83863dadce338224c10089c7811f5f9e282b8b0
Joshua Howard <j_h556@txstate.edu>
2025-09-26 14:45:41 -0500

    File upload for Simple demo

--- demos/src/simple/definitions/prompts/residence.prompts.ts
+++ demos/src/simple/definitions/prompts/residence.prompts.ts
@@ -1,5 +1,6 @@
 import { type PromptDefinition } from '@reqquest/api'
-import { type MutationMessage, MutationMessageType } from '@txstate-mws/graphql-server'
+import { type MutationMessage, MutationMessageType, UploadFiles } from '@txstate-mws/graphql-server'
+import { fileHandler } from 'fastify-txstate'
 import { StateResidencePromptSchema, StateResidencePromptData, StateResidenceConfigRequirementData, StateResidenceConfirmationPromptSchema } from '../models/index.js'
 
 
@@ -8,6 +9,30 @@ export const state_residence_prompt: PromptDefinition = {
   title: 'State residency',
   description: 'Applicant will identify if they reside in the required state.',
   schema: StateResidencePromptSchema,
+  preload: (data, config, allConfig) => {
+    console.log(`**preload data: ${JSON.stringify(data)}`)
+    console.log(`**preload config: ${JSON.stringify(config)}`)
+    console.log(`**preload allConfig: ${JSON.stringify(allConfig)}`)
+  },
+  preValidate: (data, config) => {
+    const messages: MutationMessage[] = []
+    const doc = data['residentIdDoc']
+    if (doc) {
+      if (doc.size > 10 * 1024 * 1024) messages.push({ type: MutationMessageType.error, message: 'This document is too large, please upload a file less than 10MB.', arg: 'residentIdDoc' })
+      if (doc.mime !== 'image/jpeg') messages.push({ type: MutationMessageType.error, message: 'File must be of type .jpeg or .jpg', arg: 'residentIdDoc' })     
+    }    
+    return messages
+  },
+  preProcessData: async (data, ctx) => {
+    if (data.residentIdDoc) {
+      for await (const file of ctx.files()){ 
+        const { checksum, size } = await fileHandler.put(file.stream) 
+        data.residentIdDoc.shasum = checksum
+        break; // easy out for single file upload
+      }      
+    }     
+    return data
+  },
   validate: (data, config, allConfig) => {
     const messages: MutationMessage[] = []
     if (data.residentOfRequiredState == null) {
@@ -38,12 +63,10 @@ export const state_residence_confirmation_prompt: PromptDefinition = {
   description: 'Applicant will identify if they reside in the required state.',
   schema: StateResidenceConfirmationPromptSchema,
   fetch: async (data, config, allConfig) => { 
-    console.log(`***All config for residence confirmation prompt  ${JSON.stringify(allConfig)}`)
     return { residentOfState: 'Texas' } //TODO ...this is a HACK as currently fetch does not have access to other requirement or prompt data
   },
   validate: (data, config, allConfig) => {
     const messages: MutationMessage[] = []
-    console.log(`**Validate all config ${JSON.stringify(allConfig)}`)
     //TODO ...this is a HACK as currently validate does not have access to other requirement or prompt data .. hardcoded TEXAS below
     if (data.residentOfRequiredState == null) {
       messages.push({ type: MutationMessageType.error, message: `Please confirm whether applicant is a resident of Texas.`, arg: 'residentOfRequiredState' })
--- ui/src/local/simple/ResidencePromptDisplay.svelte
+++ ui/src/local/simple/ResidencePromptDisplay.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
+  import { FieldTextInput, Form } from '@txstate-mws/carbon-svelte'
   import type { StateResidencePromptData } from './types.js'
-  export let data: StateResidencePromptData
+  export let data: Partial<StateResidencePromptData>
   export let configData
   // $: resultText = `Resident homeowner of ${configData.state_residence_req.residentOfState}`  
 </script>
 {#if data.residentOfRequiredState}
-   Resident of ##configData is { configData ? '' : 'not ' } available to display state##<!-- Config data does not seem to be available for display prompt {resultText}. -->
+   <p>Resident of ##configData is { configData ? '' : 'NOT ' } available to display state##</p><!-- Config data does not seem to be available for display prompt {resultText}. -->
+   <br>
+    <label for="firstName" style="font-weight:bold;">Firstname</label><br>
+    <input type="text" id="firstName" value={data.firstName} disabled /><br><br>
+    <label for="lastName" style="font-weight:bold;">Lastname</label><br>
+    <input type="text" id="lastName" value={data.lastName} disabled /><br><br>
+    <label for="streetAddress" style="font-weight:bold;">Street address</label><br>
+    <input type="text" id="streetAddress" value={data.streetAddress} disabled /><br><br>
+    <label for="city" style="font-weight:bold;">City</label><br>
+    <input type="text" id="city" value={data.city} disabled /><br><br>
+    <label for="zipCode" style="font-weight:bold;">ZipCode</label><br>
+    <input type="text" id="zipCode" value={data.zipCode} disabled /><br><br>
 {:else}
-  Non resident.
+  <p>Non resident.</p>
 {/if}
