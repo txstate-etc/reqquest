5d82206e9c4a26a5da2a65244426e1ef0c59b4d0
kevinpena <kevinpena160@gmail.com>
2025-09-30 08:51:54 -0500

    adding enabled/disabled programs, abstracting period panel

---
+++ ui/src/lib/components/PeriodPanel.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+  import { ActionSet, Panel, TagSet } from "@txstate-mws/carbon-svelte"
+  import { Tab, TabContent, Tabs, Tag } from "carbon-components-svelte"
+  import { uiRegistry } from "../../local"
+  import { invalidate } from "$app/navigation"
+  import { api } from "$lib/api"
+  import { page } from '$app/stores'
+  import SettingsEdit from 'carbon-icons-svelte/lib/SettingsEdit.svelte'
+  import View from 'carbon-icons-svelte/lib/View.svelte'
+
+
+  export let program: any
+  export let sharedProgramRequirements: any
+  export let openModal: any
+  export let onClick: any
+  
+  const disablePeriodProgram = (requirementKey: string) => async () => {
+    const res = await api.disablePeriodProgramRequirements($page.params.id!, requirementKey, true)
+    await invalidate('api:getPeriodConfigurations')
+  }
+
+  const enablePeriodProgram = (requirementKey: string) => async () => {
+    const res = await api.disablePeriodProgramRequirements($page.params.id!, requirementKey, false)
+    await invalidate('api:getPeriodConfigurations')
+  }
+
+  $: enabledRequirements = program.requirements.filter(r => r.enabled)
+  $: disabledRequirements = program.requirements.filter(r => !r.enabled)
+
+</script>
+  <Panel title={program.title} expandable expanded>
+    <Tabs autoWidth>
+      <Tab label={`Enabled Requirements (${enabledRequirements.length})`} />
+      <Tab label={`Disabled Requirements (${disabledRequirements.length})`} />
+      <svelte:fragment slot='content'>
+        <TabContent>
+          {#each enabledRequirements as requirement (requirement.key)}
+            {@const reqDef = uiRegistry.getRequirement(requirement.key)}
+            <Panel title={requirement.title} expandable noPrimaryAction actions={[{ label: 'Configure requirement', onClick: onClick('requirement', requirement), disabled: reqDef.configureComponent == null || !requirement.configuration.actions.update }, { label: 'Disable Requirement', onClick: disablePeriodProgram(requirement.key) }]}>
+              <div style="display: content" slot="headerLeft">
+                <TagSet tags={[{ label: 'Requirement', type: 'yellow' }]} />
+              </div>
+              <!-- <Button on:click={onClick('requirement', requirement)} type="primary" size="small" icon={SettingsEdit} iconDescription="Edit Configuration" disabled={reqDef.configureComponent == null || !requirement.configuration.actions.update} />  -->
+            <div style="display: content" slot="headerRight">
+              {@const tags = sharedProgramRequirements[requirement.key]?.length > 1  ? [{ label: 'Shared', onClick: openModal(requirement.key) }] : []}
+              <TagSet tags={tags} />
+            </div>
+
+             <ul class="prompts">
+                {#each requirement.prompts as prompt (prompt.key)}
+                {@const promptDef = uiRegistry.getPrompt(prompt.key)}
+                <li class="prompt justify-between">
+                  <span>
+                    <Tag type='green'>Prompt</Tag>{prompt.title}
+                  </span>
+                  <ActionSet
+                    actions={[
+                      { label: 'View', icon: View },
+                      { label: 'settings', icon: SettingsEdit, disabled: promptDef.configureComponent == null || !prompt.configuration.actions.update, onClick: onClick('prompt', prompt) }
+                    ]}
+                  />
+                </li>
+                {/each}
+              </ul>
+            </Panel>
+          {/each}
+        </TabContent>
+        <TabContent>
+          {#each disabledRequirements as requirement (requirement.key)}
+            <Panel title={requirement.title} actions={[{ label: 'Enable Requirement', onClick: enablePeriodProgram(requirement.key) }]}>
+              Requirement: {requirement.title}
+              <ul class="prompts">
+                {#each requirement.prompts as prompt (prompt.key)}
+                  <li class="prompt">
+                    Prompt: {prompt.title}
+                  </li>
+                {/each}
+              </ul>
+            </Panel>
+          {/each}
+        </TabContent>
+      </svelte:fragment>
+    </Tabs>
+  </Panel>
--- ui/src/routes/periods/[id]/configure/+page.svelte
+++ ui/src/routes/periods/[id]/configure/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import { Panel, PanelFormDialog, Card, ColumnList, ActionSet, TagSet } from '@txstate-mws/carbon-svelte'
   import { Accordion, AccordionItem, Button, InlineNotification, NotificationActionButton, Tab, TabContent, Tabs, Tag } from 'carbon-components-svelte'
-  import SettingsEdit from 'carbon-icons-svelte/lib/SettingsEdit.svelte'
-  import View from 'carbon-icons-svelte/lib/View.svelte'
+
   import { invalidate } from '$app/navigation'
   import { page } from '$app/stores'
   import { api, type Period } from '$lib'
   import type { PageData } from './$types'
   import { uiRegistry } from '../../../../local'
   import { Modal } from 'carbon-components-svelte'
+    import PeriodPanel from '$lib/components/PeriodPanel.svelte'
 
   export let data: PageData
   $: ({ programs, period } = data)
@@ -64,15 +64,7 @@
     closeConfigurationDialog()
   }
   
-  const disablePeriodProgram = (requirementKey: string) => async () => {
-    const res = await api.disablePeriodProgramRequirements($page.params.id!, requirementKey, true)
-    await invalidate('api:getPeriodConfigurations')
-  }
 
-  const enablePeriodProgram = (requirementKey: string) => async () => {
-    const res = await api.disablePeriodProgramRequirements($page.params.id!, requirementKey, false)
-    await invalidate('api:getPeriodConfigurations')
-  }
 
   const openModal = (key: string) => () => {
     sharedModal = { open: true, requirementKey: key }
@@ -86,6 +78,9 @@
     await invalidate('api:getPeriodConfigurations')
   }
 
+  $:enabledPrograms = programs?.filter(p => p.enabled)
+  $:disabledPrograms = programs?.filter(p => !p.enabled)
+
 </script>
 
 <!-- Configuring Pe{program.title}riod: {period.name}{#if period.code} ({period.code}){/if} -->
@@ -98,62 +93,22 @@
   </InlineNotification>
 {/if}
 
-{#each programs as program (program.key)}
-  <Panel title={program.title} expandable expanded>
-    <Tabs>
-      <Tab label={`Enabled Programs`} />
-      <Tab label={`Disabled Programs`} />
-      <svelte:fragment slot='content'>
-        <TabContent>
-          {#each program.requirements.filter(r => r.enabled) as requirement (requirement.key)}
-            {@const reqDef = uiRegistry.getRequirement(requirement.key)}
-            <Panel title={requirement.title} expandable noPrimaryAction actions={[{ label: 'Configure requirement', onClick: onClick('requirement', requirement), disabled: reqDef.configureComponent == null || !requirement.configuration.actions.update }, { label: 'Disable Requirement', onClick: disablePeriodProgram(requirement.key) }]}>
-              <div style="display: content" slot="headerLeft">
-                <TagSet tags={[{ label: 'Requirement', type: 'yellow' }]} />
-              </div>
-              <!-- <Button on:click={onClick('requirement', requirement)} type="primary" size="small" icon={SettingsEdit} iconDescription="Edit Configuration" disabled={reqDef.configureComponent == null || !requirement.configuration.actions.update} />  -->
-            <div style="display: content" slot="headerRight">
-              {@const tags = sharedProgramRequirements[requirement.key]?.length > 1  ? [{ label: 'Shared', onClick: openModal(requirement.key) }] : []}
-              <TagSet tags={tags} />
-            </div>
-
-             <ul class="prompts">
-                {#each requirement.prompts as prompt (prompt.key)}
-                {@const promptDef = uiRegistry.getPrompt(prompt.key)}
-                <li class="prompt justify-between">
-                  <span>
-                    <Tag type='green'>Prompt</Tag>{prompt.title}
-                  </span>
-                  <ActionSet
-                    actions={[
-                      { label: 'View', icon: View },
-                      { label: 'settings', icon: SettingsEdit, disabled: promptDef.configureComponent == null || !prompt.configuration.actions.update, onClick: onClick('prompt', prompt) }
-                    ]}
-                  />
-                </li>
-                {/each}
-              </ul>
-            </Panel>
-          {/each}
-        </TabContent>
-        <TabContent>
-          {#each program.requirements.filter(r => !r.enabled) as requirement (requirement.key)}
-            <Panel title={requirement.title} actions={[{ label: 'Enable Requirement', onClick: enablePeriodProgram(requirement.key) }]}>
-              Requirement: {requirement.title}
-              <ul class="prompts">
-                {#each requirement.prompts as prompt (prompt.key)}
-                  <li class="prompt">
-                    Prompt: {prompt.title}
-                  </li>
-                {/each}
-              </ul>
-            </Panel>
-          {/each}
-        </TabContent>
-      </svelte:fragment>
-    </Tabs>
-  </Panel>
-{/each}
+<Tabs autoWidth>
+  <Tab label={`Enabled Programs (${enabledPrograms.length})`} />
+  <Tab label={`Disabled Programs (${disabledPrograms.length})`} />
+  <svelte:fragment slot='content'>
+    <TabContent>
+      {#each enabledPrograms as program (program.key)}
+        <PeriodPanel {program} {sharedProgramRequirements} {openModal} {onClick} />
+      {/each}
+    </TabContent>
+    <TabContent>
+      {#each disabledPrograms as program (program.key)}
+        <PeriodPanel {program} {sharedProgramRequirements} {openModal} {onClick} />
+      {/each}
+    </TabContent>
+  </svelte:fragment>
+</Tabs>
 
 {#if editingConfiguration && editingConfigurationDef != null}
   {#if editingConfigurationType === 'prompt'}
