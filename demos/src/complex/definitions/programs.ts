61cb6fd9584aa296496d0882a92472963cf61f3d
James Dougan <j_h556@txstdeb49.aychoh.local>
2025-10-09 09:04:23 -0500

    Complex demo baseline construct

---
+++ demos/src/complex/definitions/programs.ts
@@ -0,0 +1,22 @@
+import type { ProgramDefinition } from '@reqquest/api'
+
+export const adopt_a_dog_program: ProgramDefinition = {
+  key: 'adopt_a_dog_program',
+  title: 'Adopt a Dog',
+  requirementKeys: [   
+  ]
+}
+
+export const adopt_a_cat_program: ProgramDefinition = {
+  key: 'adopt_a_cat_program',
+  title: 'Adopt a Cat',
+  requirementKeys: [    
+  ]
+}
+
+export const foster_a_pet_program: ProgramDefinition = {
+  key: 'foster_a_pet_program',
+  title: 'Foster a Pet',
+  requirementKeys: [    
+  ]
+}
