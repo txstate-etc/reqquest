4a96766a115090d6241d4ba80487e4b24856159f
Charles Tabor <ct37@txstate.edu>
2025-10-16 20:30:10 -0500

    ui move the admin user column and row generation out of svelte and into the page code.

--- ui/src/routes/roles/users/+page.svelte
+++ ui/src/routes/roles/users/+page.svelte
@@ -1,20 +1,16 @@
 <script lang="ts">
-  import { ColumnList, FieldMultiselect, FilterUI, type ColumnDefinition } from '@txstate-mws/carbon-svelte'
+  import { ColumnList, FieldMultiselect, FilterUI } from '@txstate-mws/carbon-svelte'
   import type { PageData } from './$types'
   import { Pagination } from '@txstate-mws/carbon-svelte'
   import { DateTime } from 'luxon';
-    import type { Groupings } from '$lib';
+  import type { Groupings } from '$lib';
 
   export let data: PageData
-  $: ({ users, availableApplicationRoles, pageInfo } = data)
-  $: page = pageInfo?.currentPage
-  $: totalItems = pageInfo?.totalItems
-  $: groupings = pageInfo?.groupings
+  $: ({ columns, rows, page, totalItems, availableApplicationRoles } = data)
 
   function handlePagination(event: CustomEvent<{ page: number, pageSize: number, totalItems: number, groupings: Groupings[]}>) {
-    page = event.detail.page
-    totalItems = event.detail.totalItems
-    groupings = event.detail.groupings
+    // event: {"isTrusted": false}
+    console.debug(`handlePagination event: ${JSON.stringify(event)}`)
   }
 
   // TODO: Dynamically add groupings
@@ -25,54 +21,7 @@
     [key: string]: string | string[] | undefined
   }
   let usersSearchFormData: UsersSearchForm | undefined
-
-  // TODO: Dynamically add groupings
-  interface UsersSearchDisplay {
-    // Local data
-    id: string
-    fullname: string
-    applicationRoles: string[]
-    groups: string[]
-    // Remote data
-    email?: string
-    otherId?: string
-    otherInfo?: any
-    // institutionalRoles?: string[]
-    // lastLogin?: string
-    // [key: string]: string | string[] | undefined
-  }
   
-  const columns: ColumnDefinition<UsersSearchDisplay>[] = [
-    { id: 'contact', label: 'Name', render: user => user['email'] ? user['fullname'] + '<br>' + user['email'] : user['fullname'] },
-    { id: 'id', label: 'IDs', render: user => [user['id'], user['otherId']].filter(id => id != null).join('<br>') },
-    { id: 'applicationRoles', label: 'Application Roles', render: user => user['applicationRoles'].join(', ') },
-    { id: 'groups', label: 'Groups', render: user => user['groups'].join(', ') }
-    // TODO: Dynamically pull extra grouping columns
-    // { id: 'institutionalRoles', label: 'Institutional Roles', render: user => user['institutionalRoles'] ? user['institutionalRoles'].join(', ') : '' },
-    // { id: 'lastLogin', label: 'Last Login', get: 'lastLogin' }
-  ]
-  for (const group of pageInfo?.groupings ?? []) {
-    if (group.useInList) columns.push({ id: group.label, label: group.displayLabel ? group.displayLabel : group.label, render: user => (!user.otherInfo || user.otherInfo[group.label] == undefined) ? '' : (Array.isArray(user.otherInfo[group.label])) ? user.otherInfo[group.label].join(', ') : user.otherInfo[group.label].toString() })
-    // if (group.useInFilters) 
-  }
-
-  function transformFromAPI (users: PageData['users']): UsersSearchDisplay[] {
-    return users.map(u => ({
-      id: u.login,
-      fullname: u.fullname,
-      // Technically a person may belong to multiple rows
-      // However the API schema shows single role.
-      // and is returning undefined causing API to return 500
-      // if applicaton roles field is requested.
-      applicationRoles: u.roles.map(r => r.name),
-      groups: u.groups,
-      // External Data
-      email: u.otherInfo?.email,
-      otherId: u.otherInfo?.otherId
-      // institutionalRoles: u.otherInfo?.institutionalRoles ?? [],
-      // lastLogin: u.otherInfo?.lastLogin ? DateTime.fromISO(u.otherInfo?.lastLogin).toFormat('MM/dd/yyyy') : 'unknown'
-    }))
-  }
 </script>
 
 <FilterUI
@@ -87,8 +36,8 @@
 <ColumnList
   title='Users'
   columns={columns}
-  rows={transformFromAPI(users)}
+  rows={rows}
 >
 </ColumnList>
-<Pagination {page} {totalItems} on:update={handlePagination} noun="user" nounPlural="users" />
+<Pagination {page} {totalItems} on:update={handlePagination} on:mount={handlePagination} noun="user" nounPlural="users" />
 
--- ui/src/routes/roles/users/+page.ts
+++ ui/src/routes/roles/users/+page.ts
@@ -1,5 +1,5 @@
 import { api } from '$lib'
-import { extractMergedFilters, extractPaginationParams } from '@txstate-mws/carbon-svelte'
+import { extractMergedFilters, extractPaginationParams, type ColumnDefinition } from '@txstate-mws/carbon-svelte'
 import type { PageLoad } from './$types'
 import type { AccessUserFilter, Pagination } from '$lib'
 
@@ -21,6 +21,57 @@ export const load: PageLoad = async ({ url, depends }) => {
   // }
   // if (groupings.size > 0) accessUsersFilter.otherGroupingsByLabel = Array.from(groupings).map(g => ({ label: g[0], ids: g[1] }))
   const { users, pageInfo } = await api.getAccessUsers(accessUsersFilter, pageFilter)
+
+  interface User {
+    // Local data
+    id: string
+    // ids = id + otherId
+    ids: string
+    // contact = fullname + email
+    contact: string
+    applicationRoles: string
+    groups: string
+    // Remote data
+    // otherInfo?: any
+    // institutionalRoles?: string[]
+    // lastLogin?: string
+    [key: string]: string | undefined
+  }
+  const rows = users.map(u => {
+    const user: User = {
+      id: u.login,
+      ids: [u.login, u.otherInfo?.otherId].filter(id => id != null).join('<br>'),
+      contact: u.otherInfo?.email ? u.fullname + '<br>' + u.otherInfo.email : u.fullname,
+      applicationRoles: u.roles.map(r => r.name).join(', '),
+      groups: u.groups.join(', ')
+      // otherInfo: u.otherInfo
+      // institutionalRoles: u.otherInfo?.institutionalRoles ?? [],
+      // lastLogin: u.otherInfo?.lastLogin ? DateTime.fromISO(u.otherInfo?.lastLogin).toFormat('MM/dd/yyyy') : 'unknown'
+    }
+    if (u.otherInfo) {
+      for (const group of pageInfo?.groupings ?? []) {
+        const label = group.label
+        if (group.useInList) user[label] = Array.isArray(u.otherInfo[label]) ? u.otherInfo[label].join(', ') : u.otherInfo[label].toString()
+      }
+    }
+    return user
+  })
+
+  const columns: ColumnDefinition<User>[] = [
+    { id: 'contact', label: 'Name', get: 'contact' },
+    { id: 'id', label: 'IDs', get: 'ids' },
+    { id: 'applicationRoles', label: 'Application Roles', get: 'applicationRoles' },
+    { id: 'groups', label: 'Groups', get: 'groups' }
+    // TODO: Dynamically pull extra grouping columns
+    // { id: 'institutionalRoles', label: 'Institutional Roles', render: user => user['institutionalRoles'] ? user['institutionalRoles'].join(', ') : '' },
+    // { id: 'lastLogin', label: 'Last Login', get: 'lastLogin' }
+  ]
+  for (const group of pageInfo?.groupings ?? []) {
+    const label = group.label
+    if (group.useInList) columns.push({ id: label, label: group.displayLabel ? group.displayLabel : label, get: label })
+    // if (group.useInFilters)
+  }
+
   depends('api:getAccessUsers')
-  return { users, pageInfo }
+  return { columns, rows, page: pageInfo?.currentPage, totalItems: pageInfo?.totalItems }
 }
