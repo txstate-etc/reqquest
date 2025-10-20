10b7cabf8f2c2bed8332b1a67caa45191437959c
Brian West <kny38@txstate.edu>
2025-10-20 09:54:33 -0500

    ui add TagSet to IntroPanel

--- ui/src/lib/components/IntroPanel.svelte
+++ ui/src/lib/components/IntroPanel.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
+  import { TagSet } from '@txstate-mws/carbon-svelte'
+  import type { TagItem } from '@txstate-mws/carbon-svelte'
+
   export let title = ''
   export let subtitle = ''
+  export let tags: TagItem[] | undefined = undefined
 </script>
 
 <section class="bg-[var(--cds-ui-01)] p-4 text-2xl">
   <div class="repel">
     <div class="max-w-lg">
       {#if title}
+      <div class="title-block [ flex gap-3 ] ">
         <h2 class="text-[1.25rem] text-[var(--cds-text-01,#1A1A1A)] font-normal leading-7">
           {title}
         </h2>
+        {#if tags}
+          <TagSet tags={tags} />
+        {/if}
+        </div>
         {#if subtitle}
-          <p class="text-sm text-[var(--cds-text-02)] leading-[18px] tracking-[0.16px]">
+          <p class="text-sm text-[var(--cds-text-02)] leading-[18px] tracking-[0.16px] pt-2">
             {subtitle}
           </p>
         {/if}
