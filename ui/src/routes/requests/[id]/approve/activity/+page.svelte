59868fc400d5ff4b2df461462d7cc30a6d3f9e10
kevinpena <kevinpena160@gmail.com>
2026-01-07 14:27:46 -0600

    add csv download to acvitity log

--- ui/src/routes/requests/[id]/approve/activity/+page.svelte
+++ ui/src/routes/requests/[id]/approve/activity/+page.svelte
@@ -5,11 +5,24 @@
   import { uiRegistry } from '../../../../../local'
   import type { PageData } from './$types'
   import ApproveLayout from '../ApproveLayout.svelte'
+  import { DateTime } from 'luxon'
+  import DocExport from 'carbon-icons-svelte/lib/DocumentExport.svelte'
+  import { downloadCsv } from '$lib/csv'
+
 
   export let data: PageData
   $: ({ activity, basicRequestData } = data)
   $: activityActions = sortby(unique(activity.map(a => ({ value: a.action })), 'value'), 'value')
   $: activityPeople = sortby(unique(activity.map(a => ({ value: a.user.login, label: `${a.user.fullname} (${a.user.login})` })), 'value'), 'label')
+
+  function formatCSVData (d: any[]) {
+    return d.map(row => ({
+      User: `${row.user.fullname} (${row.user.login})` + (row.impersonatedBy ? `<div class="impersonator">${htmlEncode(row.impersonatedBy.fullname)} (${row.impersonatedBy.login})</div>` : ''),
+      'Created At': longDateTime(row.createdAt),
+      Action: row.action,
+      Description: row.description
+    }))
+  }
 </script>
 
 <ApproveLayout {basicRequestData}>
@@ -23,12 +36,21 @@
       <FieldDate path="happenedBefore" labelText="To" placeholder="Activities Before" />
     </FilterUI>
 
-    <ColumnList title='Activity Log' rows={activity} columns={[
-      { id: 'user', label: 'Person', render: row => htmlEncode(`${row.user.fullname} (${row.user.login})`) + (row.impersonatedBy ? `<div class="impersonator">${htmlEncode(row.impersonatedBy.fullname)} (${row.impersonatedBy.login})</div>` : '') },
-      { id: 'createdAt', label: 'Date', render: row => longDateTime(row.createdAt) },
-      { id: 'action', label: 'Activity', get: 'action' },
-      { id: 'description', label: 'Detail', get: 'description' }
-    ]} noItemsTitle="No activity." noItemsSubtitle="No activity has been recorded for this {uiRegistry.getWord('appRequest').toLocaleLowerCase()}." />
+    <ColumnList
+      searchable
+      title='Activity Log'
+      rows={activity}
+      columns={[
+        { id: 'user', label: 'Person', render: row => htmlEncode(`${row.user.fullname} (${row.user.login})`) + (row.impersonatedBy ? `<div class="impersonator">${htmlEncode(row.impersonatedBy.fullname)} (${row.impersonatedBy.login})</div>` : '') },
+        { id: 'createdAt', label: 'Date', render: row => longDateTime(row.createdAt) },
+        { id: 'action', label: 'Activity', get: 'action' },
+        { id: 'description', label: 'Detail', get: 'description' }
+      ]}
+      listActions={[
+        { label: 'Download', icon: DocExport, onClick: () => { downloadCsv(formatCSVData(data.activity), 'app_requests_activity_log') } }
+      ]}
+      noItemsTitle="No activity."
+      noItemsSubtitle="No activity has been recorded for this {uiRegistry.getWord('appRequest').toLocaleLowerCase()}." />
   </div>
   <Pagination
     page={data.pageInfo?.currentPage}
