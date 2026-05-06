8958a9e059a397459fb7d34b8f417be5b1ef5573
Joshua Howard <j_h556@txstate.edu>
2026-05-06 17:52:45 -0500

    Naming update for coalesced app data

--- ui/src/routes/requests/[id]/approve/[programKey]/+page.ts
+++ ui/src/routes/requests/[id]/approve/[programKey]/+page.ts
@@ -8,8 +8,8 @@ export const load: PageLoad = async ({ params, depends }) => {
   //TODO: Implement prestage check and call if needed for prompts
   const inlinePrompts = getInlineReviewerEditPrompts(appRequest)
   const inlinePromptsWithData = await api.getPromptDataLegion(params.id, inlinePrompts.map(prompt => prompt.id))
-  const coalescedAppRequestData = coalesceAppRequestPrompts(appRequest, inlinePromptsWithData)
+  const coalescedAppRequest = coalesceAppRequestPrompts(appRequest, inlinePromptsWithData)
   depends('request:approve')
   if (!appRequest) throw error(404, 'App Request not found')
-  return { appRequest: coalescedAppRequestData, programKey: params.programKey }
+  return { appRequest: coalescedAppRequest, programKey: params.programKey }
 }
