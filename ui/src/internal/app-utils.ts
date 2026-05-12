ca932e681f1979c5b72db34c6805b5e1430eef1d
Joshua Howard <j_h556@txstate.edu>
2026-05-12 14:04:09 -0500

    Removal of unused function in app-utils

--- ui/src/internal/app-utils.ts
+++ ui/src/internal/app-utils.ts
@@ -1,15 +1,3 @@
-export const getAppIdsWithSharedPrompts = (appRequest, application) => {
-  const appsWithSharedPrompts: any[] = []  
-  for (const app of appRequest.applications) {
-    if (app.id == application.id) continue
-    const sharedPrompts = app.requirements.flatMap(req => req.prompts.filter(prompt => application.requirements.some(r => r.prompts.some(p => p.key === prompt.key))))
-    if (sharedPrompts.length > 0) {
-      appsWithSharedPrompts.push(app)
-    }
-  }
-  return appsWithSharedPrompts
-}
-
 export const excludeAppsByReqTypesAndStatus = (appRequests, requirementType: string[], requirementStatus: string[]) => {
   for (const ar of appRequests){
     for (let x = ar.applications.length - 1; x >= 0; x--){
