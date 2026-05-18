c6666be00c54dd57b9880fa73a46c2797ad966e3
Joshua Howard <j_h556@txstate.edu>
2026-05-18 14:17:48 -0500

    Applicant dash trigger invalidate 'api:getAccess'.  Append depends to root layouts.ts

--- ui/src/routes/+layout.ts
+++ ui/src/routes/+layout.ts
@@ -5,6 +5,7 @@ import { unifiedAuth } from '@txstate-mws/sveltekit-utils'
 export const load: LayoutLoad = async input => {
   api.fetch = input.fetch
   await unifiedAuth.handle(api, input)
+  input.depends('api:getAccess')
   const access = await api.getAccess()
   return { access }
 }
