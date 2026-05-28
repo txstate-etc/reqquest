1dc4a697c352a806935f79cb8756085c0f4daa40
kevinpena <kevinpena160@gmail.com>
2026-05-28 08:13:01 -0500

    updating Modal to PanelFormDialog

--- ui/src/internal/components/ApplicantOptOutModal.svelte
+++ ui/src/internal/components/ApplicantOptOutModal.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Form } from '@txstate-mws/carbon-svelte'
+  import { Form, PanelFormDialog } from '@txstate-mws/carbon-svelte'
   import type { FormStore } from '@txstate-mws/svelte-forms'
   import { Modal } from 'carbon-components-svelte'
   import { afterNavigate, invalidate, invalidateAll } from '$app/navigation'
@@ -19,10 +19,13 @@
 
   let store: FormStore | undefined
 
-  async function onSubmit (data: any) {
+  async function submit (data: any) {
     loading = true
     const { success, messages } = await api.updatePrompt(prompt.id, data, false)
     data = {}
+    open = false
+    loading = false
+    invalidateAll()
     return {
       success,
       messages,
@@ -51,28 +54,19 @@
   <Loading />
 {/if}
 
-<Modal
-  bind:open
-  modalHeading={`${optIn ? 'Opt in to' : 'Opt out of'} ${prompt?.title}?`}
-  primaryButtonText={optIn ? 'Opt in' : 'Opt out'}
-  secondaryButtonText="Cancel"
-  primaryButtonDisabled={loading}
-  on:click:button--primary={async () => {
-    await store?.submit()
-    open = false
-    loading = false
-    invalidateAll()
-  }}
-  on:click:button--secondary={() => { open = false }}
-  on:close={() => { prompt = {} }}
-  class='opt-out-modal'
->
-  {#key prompt.id}
-    <Form bind:store hideFallbackMessage unsavedWarning submit={onSubmit} validate={onValidate} preloadAsDraft={!prompt.hasSavedData} preload={prompt.preloadData} let:data>
-      <svelte:component this={def!.formComponent} {data} appRequestId={appRequest.id} appRequestData={appRequest.data} fetched={prompt.fetchedData} configData={prompt.configurationData} gatheredConfigData={prompt.gatheredConfigData} store={store} />
-      <svelte:fragment slot="submit" let:submitting>
-      </svelte:fragment>
-    </Form>
-  {/key}
-</Modal>
-
+<PanelFormDialog
+  let:data
+  bind:store
+  centered
+  open={open}
+  on:cancel={() => { open = false }}
+  on:validate={onValidate}
+  {submit}
+  title={`${optIn ? 'Opt in to' : 'Opt out of'} ${prompt?.title}?`}
+  submitText={optIn ? 'Opt in' : 'Opt out'}
+  cancelText="Cancel"
+  preload={prompt.preloadData}
+  preloadAsDraft={!prompt.hasSavedData} 
+  >
+    <svelte:component this={def!.formComponent} {data} appRequestId={appRequest.id} appRequestData={appRequest.data} fetched={prompt.fetchedData} configData={prompt.configurationData} gatheredConfigData={prompt.gatheredConfigData} {store} />
+</PanelFormDialog>
