8a46750177aa8f4213cd2ec45dbb1e396b51886f
Joshua Howard <j_h556@txstate.edu>
2026-05-29 16:49:04 -0500

    Cleanup models post AJV coercion being removed.  Temp fix for unsaved trigger on FileUploads (#43)

--- ui/src/internal/components/ApplicantPromptPage.svelte
+++ ui/src/internal/components/ApplicantPromptPage.svelte
@@ -23,6 +23,7 @@
   export let data: PageData
   $: ({ prompt, appRequestForExport, dataVersion } = data)
   $: def = uiRegistry.getPrompt(prompt.key)
+  $: unsavedWarningCheck = true // TODO: Temp fix to prevent unsavedWarn check from triggering after save and continue
   const nextHref = getContext<Writable<{ nextHref: ResolvedPathname, prevHref: ResolvedPathname | undefined }>>('nextHref')
 
   let store: FormStore | undefined
@@ -55,6 +56,7 @@
   }
 
   async function onSaved () {
+    unsavedWarningCheck = false
     await invalidate('request:apply')
     if (continueAfterSave && prompt.answered) {
       // eslint-disable-next-line svelte/no-navigation-without-resolve -- already resolved
@@ -64,6 +66,7 @@
       await store?.setData(appRequestForExport.data[prompt.key] as object)
     }
     loading = false
+    unsavedWarningCheck = true
   }
 
   let lastPromptId: string | undefined
@@ -88,7 +91,7 @@
     <h2 id="prompt-title" tabindex="-1" autofocus class="font-medium text-xl text-center">{prompt.title}</h2>
     <p class="text-center"> {prompt.description}</p>
   </div>
-  <Form bind:store hideFallbackMessage unsavedWarning submit={onSubmit} validate={onValidate} preloadAsDraft={!prompt.hasSavedData} preload={prompt.preloadData}  on:saved={onSaved} let:data>
+  <Form bind:store hideFallbackMessage unsavedWarning={unsavedWarningCheck} submit={onSubmit} validate={onValidate} preloadAsDraft={!prompt.hasSavedData} preload={prompt.preloadData}  on:saved={onSaved} let:data>
     <svelte:component this={def!.formComponent} {data} appRequestId={appRequestForExport.id} appRequestData={appRequestForExport.data} fetched={prompt.fetchedData} configData={prompt.configurationData} gatheredConfigData={prompt.gatheredConfigData} />
     <svelte:fragment slot="submit" let:submitting>
       <div class='form-submit flex gap-12 justify-center mt-16'>
--- ui/src/local/rc/MaintainSysDocumentation.svelte
+++ ui/src/local/rc/MaintainSysDocumentation.svelte
@@ -7,6 +7,5 @@
 
 <FieldRadio boolean path="maintainSysDocumentation" legendText="Have you ever created a guide, a readme file, or a set of instructions to help someone else use a tool or complete a task?" items={[{ label: 'Yes', value: true }, { label: 'No', value: false }]} />
 
-{#if data.maintainSysDocumentation}
-  <FieldUpload path='documentation' labelText='Upload a file of your documentation' />
-{/if}
+<FieldUpload path='documentation' labelText='Upload a file of your documentation' conditional={data.maintainSysDocumentation===true} />
+
