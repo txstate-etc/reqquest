da4de4680e70cc52ed7c5fa02089cd33cb086a6a
kevinpena <kevinpena160@gmail.com>
2026-06-04 07:59:36 -0500

    adding notification for returned back to aplicant txstate-etc/reqquest-txstate#328

--- api/src/util/notifications.ts
+++ api/src/util/notifications.ts
@@ -5,9 +5,17 @@ type NotificationCB = (ctx: RQContext, appRequest: AppRequest, programKey: strin
 
 export const internalNotifications: NotificationCB[] = [
   async (ctx, ar, programKey, oldPhase) => {
+    // Review complete
     if (ar.phase === AppRequestPhase.COMPLETE) {
       const user = await ctx.svc(AccessUserService).findByInternalId(ar.userInternalId)
       if (user?.email) await ctx.svc(MailService).sendmulti({ users: [user?.email], templateKey: 'ReviewComplete', extra: {} })
+    }
+  },
+  async (ctx, ar, programKey, oldPhase) => {
+    // Returned back to applicant
+    if (oldPhase === ApplicationPhase.APPROVAL && ar.phase === AppRequestPhase.STARTED) {
+      const user = await ctx.svc(AccessUserService).findByInternalId(ar.userInternalId)
+      if (user?.email) await ctx.svc(MailService).sendmulti({ users: [user?.email], templateKey: 'ApplicantReturn', extra: {} })
     }
   }
 ]
