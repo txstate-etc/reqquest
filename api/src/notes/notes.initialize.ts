1d54a3261af5f4338b922f853f6e2f15236feec4
Charles Tabor <ct37@txstate.edu>
2026-07-10 11:14:39 -0500

    Add a way for search to filter on notes txstate-etc/reqquest-txstate#253 (#69)
    * Fulltext Index db notes content and add search if can view reviewer page where notes are managed. txstate-etc/reqquest-txstate#253
    
    * Allow searchNotes filter to be undefined.
    
    * Fix stray curly bracket in note query
    
    * Add test for reviewer and applicants note search. txstate-etc/reqquest-txstate#253

--- api/src/notes/notes.initialize.ts
+++ api/src/notes/notes.initialize.ts
@@ -23,5 +23,15 @@ export const noteMigrations: DatabaseMigration[] = [
       const exists = await db.getval("SELECT 1 FROM information_schema.COLUMNS WHERE TABLE_NAME = 'app_request_notes' AND COLUMN_NAME = 'persistent'")
       if (!exists) await db.execute('ALTER TABLE app_request_notes ADD COLUMN persistent TINYINT(1) NOT NULL DEFAULT 0')
     }
+  },
+  {
+    id: '20260617120000',
+    async execute (db: Queryable) {
+      // NOTE: Requires mysql version 5.7 which our systems support (may even be supported earlier)
+      //   https://dev.mysql.com/doc/refman/5.7/en/fulltext-natural-language.html
+      // However if this is an issue we can revert to searching with LIKEs.
+      const exists = await db.getval("SELECT 1 FROM information_schema.STATISTICS WHERE TABLE_NAME = 'app_request_notes' AND INDEX_NAME = 'ft_content'")
+      if (!exists) await db.execute('ALTER TABLE app_request_notes ADD FULLTEXT INDEX ft_content (content)')
+    }
   }
 ]
