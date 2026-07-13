3e8b4b629b7ed05026d1c50bceab749274206d3d
Joshua Howard <j_h556@txstate.edu>
2026-07-13 12:39:54 -0500

    Copu update for potential eligible benefits review screen

--- ui/src/routes/requests/[id]/apply/programs/+page.svelte
+++ ui/src/routes/requests/[id]/apply/programs/+page.svelte
@@ -27,7 +27,7 @@
   })
 </script>
 
-<ProgressNavContainer title="Your potential benefits" subtitle='Select "Start" to answer additional qualifying questions about the benefits you may be eligible for.'>
+<ProgressNavContainer title="Your potential benefits" subtitle='Based on your responses for far, you may be eligible for the benefits listed below.  If you believe you qualify for additional benefits, you may need to reviews your answers, complete additional questions, or provides supporting documentation before eligibility can be confirmed.'>
   {@const eligibleApplicationsForNav = applicationsForNav.filter(a => a.ineligiblePhase == null)}
   <div class="max-w-screen-md mx-auto">    
     {#if eligibleApplicationsForNav.length > 0} 
