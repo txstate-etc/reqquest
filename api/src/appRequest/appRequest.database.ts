46b892e08f55ec5fcf09956ab653ccb04e0b0b52
Joshua Howard <j_h556@txstate.edu>
2026-07-14 13:56:39 -0500

    Remove invalidated check to prevent influence on execution requirement resolvers. Resolve is run for all functions regardless of invalidation status https://github.com/txstate-etc/reqquest-txstate/issues/377

--- api/src/appRequest/appRequest.database.ts
+++ api/src/appRequest/appRequest.database.ts
@@ -588,12 +588,12 @@ export async function evaluateAppRequest (appRequestInternalId: number, tdb?: Qu
         let hasUnanswered = false
         for (const prompt of noDisplayPrompts) {
           prompt.visibility = PromptVisibility.UNREACHABLE
-          if (!prompt.answered || ([ApplicationStatus.ELIGIBLE, ApplicationStatus.PENDING].includes(application.status) && prompt.invalidated)) hasUnanswered = true
+          if (!prompt.answered) hasUnanswered = true
           else requiredData[prompt.key] = data[prompt.key]
         }
         let resolveInfo = requirement.definition.resolve(requiredData, configLookup[requirement.definition.key] ?? {}, configLookup)
 
-        const anyOrderAllAnswered = anyOrderPrompts.every(p => p.answered && !([ApplicationStatus.ELIGIBLE, ApplicationStatus.PENDING].includes(application.status) && p.invalidated))
+        const anyOrderAllAnswered = anyOrderPrompts.every(p => p.answered)
         for (const prompt of anyOrderPrompts) {
           prompt.visibility = PromptVisibility.UNREACHABLE
           if (!hasUnanswered && resolveInfo.status === RequirementStatus.PENDING) {
@@ -614,7 +614,7 @@ export async function evaluateAppRequest (appRequestInternalId: number, tdb?: Qu
             else prompt.visibility = PromptVisibility.AVAILABLE
             promptsSeenInApplication.add(prompt.key)
             promptsSeenInRequest.add(prompt.key)
-            if (prompt.answered && !([ApplicationStatus.ELIGIBLE, ApplicationStatus.PENDING].includes(application.status) && prompt.invalidated)) {
+            if (prompt.answered) {
               requiredData[prompt.key] = data[prompt.key]
               resolveInfo = requirement.definition.resolve(requiredData, configLookup[requirement.definition.key] ?? {}, configLookup)
             } else hasUnanswered = true
