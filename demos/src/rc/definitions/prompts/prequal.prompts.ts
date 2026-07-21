2f6bac65ad29113c2d2211445b3958299176842d
Joshua Howard <j_h556@txstate.edu>
2026-07-21 09:09:02 -0500

    Prescheduled release: Correction to expiration check on signed prestage data (#87)
    * Update rq-next demo to use back to back prestage for va-cert issue replication
    
    * Expiry check calc correction on signed prestage.

--- demos/src/rc/definitions/prompts/prequal.prompts.ts
+++ demos/src/rc/definitions/prompts/prequal.prompts.ts
@@ -1,12 +1,44 @@
-import { MutationMessage, PromptDefinition } from '@reqquest/api'
-import { orgs, PreQualPromptData, PrequalPropmtSchema } from '../models/prequal.models.js'
+import { MutationMessage, PromptDefinition, PromptPreStagingRecurrence } from '@reqquest/api'
+import { orgs, PrequalLsatPrestageData, PrequalLsatPrestageSchema, PreQualPromptData, PrequalPromptSchema, PreQualUserInfoPrestagePromptData, PrequalUserInfoPrestagePropmtSchema, PreQualUserInfoPromptData, PrequalUserInfoPromptSchema } from '../models/prequal.models.js'
 import { MutationMessageType } from '@txstate-mws/graphql-server'
+
+export const pre_qual_user_info_prompt: PromptDefinition<PreQualUserInfoPromptData> = {
+  key: 'pre_qual_user_info_prompt',
+  title: 'Confirm User info',
+  description: 'Confirm personal user information',
+  prestage: {
+    recur: PromptPreStagingRecurrence.ALWAYS,
+    schema: PrequalUserInfoPrestagePropmtSchema,
+    fetch: (appRequest, config, allPeriodConfig, ctx): PreQualUserInfoPrestagePromptData  => {
+      return {
+        netid: ctx.login,
+        name: ctx.authInfo?.user?.fullname ?? ctx.login,
+        email: ctx.authInfo?.user?.email ?? `${ctx.login}@request-next.local`
+      }
+    }    
+  }, 
+  schema: PrequalUserInfoPromptSchema,
+  validate: (data, config) => {
+    const messages: MutationMessage[] = []
+    if (data.correct == null) messages.push({ type: MutationMessageType.error, message: 'Please confirm if your personal info is correct.', arg: 'correct' })
+    return messages
+  }
+}
 
 export const pre_qual_prompt: PromptDefinition<PreQualPromptData> = {
   key: 'pre_qual_prompt',
-  title: 'Pre Qualification',
-  description: 'MWS Technical Mentorship Experience Pre-Qualification',
-  schema: PrequalPropmtSchema,
+  title: 'Qualifications and expectations',
+  description: 'MWS Technical Mentorship Experience Qualifications and expectations',
+  prestage: {
+    recur: PromptPreStagingRecurrence.ALWAYS,
+    schema: PrequalLsatPrestageSchema,
+    fetch: (appRequest, config, allPeriodConfig, ctx): PrequalLsatPrestageData  => {
+      return {
+        lsat: Math.floor(Math.random() * (180 - 120 + 1)) + 120
+      }
+    }    
+  }, 
+  schema: PrequalPromptSchema,
   fetch: () => {
     return orgs
   },
---
+++ ui/src/local/rc/PreQualUserInfoPromptDisplay.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+export let data
+export let prestageData
+$: prestage = prestageData.latest ?? prestageData.current
+</script>
+
+<p>NetID: {prestage.nodes.client.data.netid}</p>
+<p>Name: {prestage.nodes.client.data.name}</p>
+<p>Email: {prestage.nodes.client.data.email}</p>
+<p>Information is correct: {data.correct ? 'Yes' : 'No'}</p>
+
+
