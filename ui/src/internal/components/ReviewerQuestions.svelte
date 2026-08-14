d1f45af02d593c80b2237ebc2b73c8757ac95cd1
Joshua Howard <j_h556@txstate.edu>
2026-08-14 11:04:04 -0500

    Force fill color for warning icon indicator (#115)
    https://github.com/txstate-etc/reqquest-txstate/issues/409

--- ui/src/internal/components/ReviewerQuestions.svelte
+++ ui/src/internal/components/ReviewerQuestions.svelte
@@ -114,7 +114,7 @@
                     {:else if promptIndicator[prompt.key]?.indicator === PromptIndicators.WARNING}
                         <WarningIconYellow />
                     {:else if promptIndicator[prompt.key]?.indicator === PromptIndicators.DISQUALIFYING}
-                        <WarningFilled size={20} class="disqualifying-icon" />
+                        <WarningFilled size={20} class="disqualifying-icon" style="fill: #da1e28;" />
                     {/if}
                     </svelte:fragment>
                     {promptIndicator[prompt.key]?.reason}
